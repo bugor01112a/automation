90df7ce2c25f2ebb26786ac694ac407a2e26d027
vadim <bugor01112a@gmail.com>
2025-03-28 09:08:19 +0300

    123

--- mytests/store_test.js
+++ mytests/store_test.js
@@ -8,14 +8,16 @@ Scenario("test registration", ({ I , objectsPage}) => {
 
   I.fillField({ xpath: '//*[@id="input-firstname"]' }, "Va");
   I.fillField({ xpath: '//*[@id="input-lastname"]' }, "5555");
-  I.fillField({ xpath: '//*[@id="input-email"]' }, "3117@gmail.com");
+  I.fillField({ xpath: '//*[@id="input-email"]' }, "311737@gmail.com");
   I.fillField({ xpath: '//*[@id="input-telephone"]' }, "1598741235");
   I.fillField({ xpath: '//*[@id="input-password"]' }, "9999999999");
   I.fillField({ xpath: '//*[@id="input-confirm"]' }, "9999999999");
+  I.click({xpath: '//*[@id="content"]/form/fieldset[3]/div/div/label[1]'});
   I.click({ xpath: '//*[@name="agree"]' });
   I.click({ xpath: '//*[@class="btn btn-primary"]' });
-  I.see("Your Account Has Been Created!");
   //pause();
+  I.see("Your Account Has Been Created!");
+  
 });
 
 
